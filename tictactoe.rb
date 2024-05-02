7553a6baf0ebe911f9702befb9a581037c0f106d
JSE955 <jordanscottedwards@gmail.com>
2024-05-02 10:36:45 -0400

    Add methods to mark board and check winner

--- tictactoe.rb
+++ tictactoe.rb
@@ -9,11 +9,48 @@ end
 
 class Board
   def initialize
-    @grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @grid = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+  end
+
+  def place_mark(mark, position)
+    case position
+    when 1 then self.grid[0][0] = mark
+    when 2 then self.grid[0][1] = mark
+    when 3 then self.grid[0][2] = mark
+    when 4 then self.grid[1][0] = mark
+    when 5 then self.grid[1][1] = mark
+    when 6 then self.grid[1][2] = mark
+    when 7 then self.grid[2][0] = mark
+    when 8 then self.grid[2][1] = mark
+    when 9 then self.grid[2][2] = mark
+    end
+  end
+
+  def check_for_winner(mark)
+    has_winner = false
+    if self.grid[0][0] == mark && self.grid[0][1] == mark && self.grid[0][2] == mark
+      has_winner = true
+    elsif self.grid[1][0] == mark && self.grid[1][1] == mark && self.grid[1][2] == mark
+      has_winner = true
+    elsif self.grid[2][0] == mark && self.grid[2][1] == mark && self.grid[2][2] == mark
+      has_winner = true
+    elsif self.grid[0][0] == mark && self.grid[1][0] == mark && self.grid[2][0] == mark
+      has_winner = true
+    elsif self.grid[0][1] == mark && self.grid[1][1] == mark && self.grid[2][1] == mark
+      has_winner = true
+    elsif self.grid[0][2] == mark && self.grid[1][2] == mark && self.grid[2][2] == mark
+      has_winner = true
+    elsif self.grid[0][0] == mark && self.grid[1][1] == mark && self.grid[2][2] == mark
+      has_winner = true
+    elsif self.grid[0][2] == mark && self.grid[1][1] == mark && self.grid[2][0] == mark
+      has_winner = true
+    end
+    has_winner
   end
 
   private
-
   attr_reader :grid
 end
 
@@ -25,13 +62,14 @@ class Game
     @player_two = player_two
     @game_board = Board.new
   end
+
+
 end
 
 puts 'Welcome to Tic-Tac-Toe!'
 puts 'Player 1 (X) - Enter your name: '
 player_one_name = gets.chomp
 player_one = Player.new(player_one_name, 'X')
-
 puts 'Player 2 (O) - Enter your name: '
 player_two_name = gets.chomp
 player_two = Player.new(player_two_name, 'O')
